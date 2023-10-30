95a4851035b75882e15dfe9d44a131ccbe9cf717
biribrinha <juliabergman523@gmail.com>
2023-10-30 19:57:21 -0300

    feat: iniciando o projeto

---
+++ src/main/java/com/mobili/api_mobili/ApiMobiliApplication.java
@@ -0,0 +1,13 @@
+package com.mobili.api_mobili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMobiliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMobiliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobili/api_mobili/ApiMobiliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobili.api_mobili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMobiliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
